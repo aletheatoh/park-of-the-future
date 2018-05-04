de3fe807bbe1810620e0254c8194d1e38978d9e1
Alethea Toh <alethea.toh@duke.edu>
2018-05-04 09:58:55 +0800

    heroku debugging

--- controllers/user.js
+++ controllers/user.js
@@ -63,7 +63,7 @@ const create = (db) => {
           response.cookie('username', request.body.username);
           response.cookie('email', request.body.email);
           response.cookie('user-id', res.rows[0].id);
-        
+
 
           // if (res.rowCount >= 1) {
           //   console.log('User created successfully');
@@ -74,9 +74,9 @@ const create = (db) => {
           //   response.cookie('user-id', res.rows[0].id);
           // }
 
-          else {
-            console.log('User could not be created');
-          }
+          // else {
+          //   console.log('User could not be created');
+          // }
 
           var context = {
             newUser: true
